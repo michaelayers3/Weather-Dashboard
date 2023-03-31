97213d92431f2b513595c05881442dc339f21a08
michaelayers3 <michaelgregoryayers@gmail.com>
2023-03-30 18:15:56 -0600

    adding some of the framework, need to start hooking up some APIs and writing a whole bunch in js.

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function getApi () {
+    var requestWeatherUrl= 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d04b52f8ed3feffd9b0a8ea7bffb3e92}'
+}
+
+function getApi2 () {
+    var GeoCodingApi= 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={d04b52f8ed3feffd9b0a8ea7bffb3e92}'
+}

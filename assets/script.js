59c5ad8285b5d9228c6d68effdd0ab910c01f4c6
michaelayers3 <michaelgregoryayers@gmail.com>
2023-04-01 17:56:00 -0600

    not sure what I'm doing. Trying to get assigned values to the card. No idea how.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var cityForm = document.getElementById('city-form');
 var cityInput;
 
+
 cityForm.addEventListener('submit', function (event) {
     event.preventDefault();
     cityInput = document.getElementById('city-input').value;
@@ -29,7 +30,7 @@ function getLatLon() {
         getWeather(city.lat, city.lon);
         saveCityData(city);
         createHistoryButton(city);
-        displayCity(city, city.time );
+        displayCity(city);
         
         
     });
@@ -53,17 +54,78 @@ function getWeather(lat, lon) {
     .then(function (data) {
         console.log(data);
         for(var i=0; i <40; i += 8) {
-            console.log(i, data.list[i]);
+            // console.log(i, data.list[i]);
         }
+      
 
-        var info = {
-            temp: (data.list[0].main.temp + '°F'),
-            wind: (data.list[0].wind.speed + ' MPH'),
-            humidity: (data.list[0].main.humidity + '%'),
-            time: (data.list[0].dt_txt),
-        };
-        console.log(info.time);
-displayWeather(info);
+        for(var i=0; i <40; i += 8) {
+            var info = {
+                time: (data.list[i].dt_txt ),
+                icon: (data.list[i].weather[0].icon),
+                temp: (data.list[i].main.temp + '°F'),
+                wind: (data.list[i].wind.speed + ' MPH'),
+                humidity: (data.list[i].main.humidity + '%'),
+            }
+            console.log(i, info);
+        }
+        var day1Display = document.getElementById('day1-display')
+        // var day2Display = document.getElementById('day2-display')
+        // var day3Display = document.getElementById('day3-display')
+        // var day4Display = document.getElementById('day4-display')
+        // var day5Display = document.getElementById('day5-display')
+
+    
+    day1Display.innerHTML = 
+    `<div>
+    <p>Time: ${info.time}</p>
+    <img src="${info.icon}">
+    <p>Temperature: ${info.temp}</p>
+    <p>Wind Speed: ${info.wind}</p>
+    <p>Humidity: ${info.humidity}</p>
+  </div>
+  `
+       
+    //     }else if (i === 8) {
+    // day2Display.innerHTML = `
+    // <div>
+    //     <p>Time: ${info.time}</p>
+    //     <img src="${info.icon}">
+    //     <p>Temperature: ${info.temp}</p>
+    //     <p>Wind Speed: ${info.wind}</p>
+    //     <p>Humidity: ${info.humidity}</p>
+    // </div>`
+    //     }else if (i === 16) {   
+    // day3Display.innerHTML = `
+    // <div>
+    //     <p>Time: ${info.time}</p>
+    //     <img src="${info.icon}">
+    //     <p>Temperature: ${info.temp}</p>
+    //     <p>Wind Speed: ${info.wind}</p>
+    //     <p>Humidity: ${info.humidity}</p>
+    // </div>`
+    //     }else if (i === 24) {   
+    // day4Display.innerHTML = `
+    // <div>
+    //     <p>Time: ${info.time}</p>
+    //     <img src="${info.icon}">
+    //     <p>Temperature: ${info.temp}</p>
+    //     <p>Wind Speed: ${info.wind}</p>
+    //     <p>Humidity: ${info.humidity}</p>
+    // </div>`
+    //     }else if (i === 32) {
+    // day5Display.innerHTML = `
+    // <div>
+    //     <p>Time: ${info.time}</p>
+    //     <img src="${info.icon}">
+    //     <p>Temperature: ${info.temp}</p>
+    //     <p>Wind Speed: ${info.wind}</p>
+    //     <p>Humidity: ${info.humidity}</p>
+    // </div>`
+    
+
+
+         displayInfo(info);
+        //  day1(info);
     });
     
 
@@ -95,8 +157,6 @@ function createHistoryButton(city) {
 //City input to show in box
 function displayCity (city) {
     var cityDisplay = document.getElementById('city-display')
-    // var time = dayjs();
-    // today.format('DD/MM/YYYY');
     cityDisplay.textContent= city.name
     
     
@@ -104,22 +164,36 @@ function displayCity (city) {
 
 
 //city temp wind and humidity to show in box
-function displayWeather (info) {
+
+function displayInfo (info) {
+    // var iconDisplay = document.getElementById('icon-display').src = 'https://openweathermap.org/img/w/' + info.icon + '.png';
+    // // var iconURL = 'https://openweathermap.org/img/w/' + info.icon + '.png';
+    // console.log(iconDisplay);
     var tempDisplay = document.getElementById('temp-display')
     tempDisplay.textContent= info.temp
+
     var windDisplay = document.getElementById('wind-display')
     windDisplay.textContent= info.wind
+
     var humidityDisplay = document.getElementById('humidity-display')
     humidityDisplay.textContent= info.humidity
+
     var timeDisplay = document.getElementById('time-display')
     timeDisplay.textContent= info.time
     
 }
 
 
-   
+
+// function day1 (info) {
+//     var day1Display = document.getElementById('day1-display')
+//     day1Display.textContent= in
+
+// }
+
                 
 //5 day forecast to show in cards in box
+
 
 // icons
 
